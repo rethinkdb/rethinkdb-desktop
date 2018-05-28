a0f7b3b53999ce5efd575cbfaa02843f65a32130
oferitz <ofer@duality.cloud>
2018-05-28 23:18:02 +0300

    fix: NewConnection route component

--- app/renderer/routes/index.js
+++ app/renderer/routes/index.js
@@ -3,13 +3,13 @@ import ErrorBoundary from 'react-error-boundary'
 import Error from '../components/error'
 import { Switch, Route } from 'react-router'
 import Home from '../views/Home'
-import Index from '../views/Connection/NewConnection'
+import NewConnection from '../views/Connection/NewConnection'
 
 export default () => (
   <ErrorBoundary FallbackComponent={Error}>
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path="/newConnection" component={Index} />
+      <Route exact path="/newConnection" component={NewConnection} />
     </Switch>
   </ErrorBoundary>
 )
