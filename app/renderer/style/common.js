6206e4c4a37d71d18aae771bc727f72e93ae2a71
Sagiv Frankel <sagiv.f@seebo.com>
2018-06-06 21:12:39 +0300

    header styling

--- app/renderer/style/common.js
+++ app/renderer/style/common.js
@@ -5,7 +5,7 @@ export default {
   mainColorLight: '#ad8cd1',
   secColor: '#F1F0F9',
   mainLightColor: '#f7f7f7',
-  mainGradient: 'linear-gradient( 180deg, #FF6FD8 10%, #3813C2 100%)',
+  mainGradient: 'linear-gradient(180deg, #FF6FD8 10%, #3813C2 100%)',
   mainBorderColor: '#d9d8ff',
   error: '#FD6585',
   info: '#4C83FF',
@@ -18,5 +18,7 @@ export default {
   grayTextColor: '#8f8e9b',
   // elements dimensions
   appHeaderHeight: '38px',
-  sideBarWidth: '90px'
+  sideBarWidth: '90px',
+  headerBackground: '#272729',
+  headerGradient: '0 1px 3px rgba(0,0,0,.2), inset 0 -2px 4px rgba(0,0,0,.15)'
 }
